efbc5570e1af26c1bef52ab8a3581366a39c9529
Damir Arh <damir.arh@gmail.com>
2020-08-04 18:47:41 +0200

    Error in asyncData method

---
+++ pages/asyncError.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component({
+  asyncData: () => {
+    return Promise.reject('Async data error')
+  },
+})
+export default class AsyncErrorPage extends Vue {}
+</script>
