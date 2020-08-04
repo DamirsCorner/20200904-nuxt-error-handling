f8d93532f7db753d28f5a63130ff1c08ded5f6ce
Damir Arh <damir.arh@gmail.com>
2020-08-04 20:13:39 +0200

    Invoke Nuxt.js error page

---
+++ pages/customError.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import { Context } from '@nuxt/types'
+
+@Component({
+  asyncData: (ctx: Context) => {
+    ctx.error({ message: 'Error fetching data...' })
+  },
+})
+export default class AsyncErrorPage extends Vue {}
+</script>
