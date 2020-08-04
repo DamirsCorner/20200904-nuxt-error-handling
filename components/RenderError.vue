be021433d3281933f38aa5d73046aae6d2b67be5
Damir Arh <damir.arh@gmail.com>
2020-08-04 19:00:25 +0200

    Rendering error

---
+++ components/RenderError.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>{{ invalid.missing }}</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class RenderError extends Vue {}
+</script>
---
+++ pages/unhandledRenderError.vue
@@ -0,0 +1,13 @@
+<template>
+  <RenderError />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import RenderError from '../components/RenderError.vue'
+
+@Component({
+  components: { RenderError },
+})
+export default class UnhandledRenderErrorPage extends Vue {}
+</script>
