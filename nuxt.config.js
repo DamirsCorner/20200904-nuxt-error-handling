11d641c7f28dfcc73d10aa75e8fceb8940ebed67
Damir Arh <damir.arh@gmail.com>
2020-08-04 19:54:55 +0200

    Browser async error handler

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,7 +34,7 @@ export default {
    ** Plugins to load before mounting the App
    ** https://nuxtjs.org/guide/plugins
    */
-  plugins: ['plugins/globalErrorHandler.ts'],
+  plugins: [{ src: 'plugins/globalErrorHandler.ts', mode: 'client' }],
   /*
    ** Auto import components
    ** See https://nuxtjs.org/api/configuration-components
--- plugins/globalErrorHandler.ts
+++ plugins/globalErrorHandler.ts
@@ -3,3 +3,7 @@ import Vue from 'vue'
 Vue.config.errorHandler = (err: Error, vm: Vue, info: string) => {
   console.log('Logged in Vue global error handler', err, vm, info)
 }
+
+window.onunhandledrejection = (event: PromiseRejectionEvent) => {
+  console.log('Logged in window.onunhandledrejection', event)
+}
