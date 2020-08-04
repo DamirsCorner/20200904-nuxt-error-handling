ebcd19bd6f008dc64efd40c62fc382be20b762d3
Damir Arh <damir.arh@gmail.com>
2020-08-04 19:21:39 +0200

    Error handling in layout

---
+++ pages/handledEventError.vue
@@ -0,0 +1,14 @@
+<template>
+  <BrokenEventHandler />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import BrokenEventHandler from '../components/BrokenEventHandler.vue'
+
+@Component({
+  components: { BrokenEventHandler },
+  layout: 'safe',
+})
+export default class UnhandledRenderErrorPage extends Vue {}
+</script>
---
+++ pages/handledRenderError.vue
@@ -0,0 +1,14 @@
+<template>
+  <RenderError />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import RenderError from '../components/RenderError.vue'
+
+@Component({
+  components: { RenderError },
+  layout: 'safe',
+})
+export default class UnhandledRenderErrorPage extends Vue {}
+</script>
