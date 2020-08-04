393ab36890de3456b42a236c7539e126ba80ec5b
Damir Arh <damir.arh@gmail.com>
2020-08-04 20:09:07 +0200

    Custom Nuxt.js error page

---
+++ layouts/error.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Custom error page: {{ error.message }}</div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+import { NuxtError } from '@nuxt/types'
+
+@Component
+export default class ErrorLayout extends Vue {
+  @Prop({ type: Object, required: true }) readonly error!: NuxtError
+}
+</script>
