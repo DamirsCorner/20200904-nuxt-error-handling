7b30a753ed737fc25d6bbebdb007994f14dc62f3
Damir Arh <damir.arh@gmail.com>
2020-08-04 19:07:38 +0200

    Error in event handler

---
+++ components/BrokenEventHandler.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="handle()">Click me!</button>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class BrokenEventHandler extends Vue {
+  handle() {
+    throw new Error('event error')
+  }
+}
+</script>
---
+++ pages/unhandledEventError.vue
@@ -0,0 +1,13 @@
+<template>
+  <BrokenEventHandler />
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import BrokenEventHandler from '../components/BrokenEventHandler.vue'
+
+@Component({
+  components: { BrokenEventHandler },
+})
+export default class UnhandledRenderErrorPage extends Vue {}
+</script>
